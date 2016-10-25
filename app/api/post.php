fff8123a74d6a267e7f9efa3f8d6293e11a4b6c1
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-25 13:21:20 +0300

    get and send post

--- app/api/post.php
+++ app/api/post.php
@@ -3,8 +3,9 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
-
-$app->get('/api/posts', function(Request $request, Response $response){
+//GET all posts
+$app->get('/api/posts', function(Request $request, Response $response)
+{
   $sql = "SELECT * FROM posts";
 
   try {
@@ -14,6 +15,51 @@ $app->get('/api/posts', function(Request $request, Response $response){
     $posts = $stmt->fetchALL(PDO::FETCH_OBJ);
     $db = null;
     echo json_encode($posts);
+
+  } catch(PDOException $e){
+    echo '{"error": {"text":'.$e->getMessage().'}}';
+  }
+});
+
+//GET Single post
+$app->get('/api/post/{id}', function(Request $request, Response $response)
+{
+  $id = $request->getAttribute('id');
+  $sql = "SELECT * FROM posts WHERE id = $id";
+
+  try {
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->query($sql);
+    $post = $stmt->fetchALL(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($post);
+
+  } catch(PDOException $e){
+    echo '{"error": {"text":'.$e->getMessage().'}}';
+  }
+});
+//ADD post
+$app->post('/api/post/add', function(Request $request, Response $response)
+{
+  $title = $request->getParam('title');
+  $category_id = $request->getParam('category_id');
+  $body = $request->getParam('body');
+
+  $sql = "INSERT INTO posts (title, category_id, body) values (:title, :category_id, :body)";
+
+  try {
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(":title", $title);
+    $stmt->bindParam(":category_id", $category_id);
+    $stmt->bindParam(":body", $body);
+
+    $stmt->execute();
+
+    echo '{"notice":{"text":"Post Added"}}';
 
   } catch(PDOException $e){
     echo '{"error": {"text":'.$e->getMessage().'}}';
