56a316217f99d3d91c2ecd64ba8baa660f7f0a49
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-25 11:14:10 +0300

    fetch posts

--- app/api/post.php
+++ app/api/post.php
@@ -5,5 +5,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app = new \Slim\App;
 
 $app->get('/api/posts', function(Request $request, Response $response){
-  echo 'POSTS';
+  $sql = "SELECT * FROM posts";
+
+  try {
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->query($sql);
+    $posts = $stmt->fetchALL(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($posts);
+
+  } catch(PDOException $e){
+    echo '{"error": {"text":'.$e->getMessage().'}}';
+  }
 });
