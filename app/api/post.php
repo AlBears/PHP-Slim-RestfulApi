b11691a6eb097af22dc4f48d1fc839425a20afaa
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-25 14:51:27 +0300

    update and delete

--- app/api/post.php
+++ app/api/post.php
@@ -65,3 +65,55 @@ $app->post('/api/post/add', function(Request $request, Response $response)
     echo '{"error": {"text":'.$e->getMessage().'}}';
   }
 });
+//Update post
+$app->put('/api/post/update/{id}', function(Request $request, Response $response)
+{
+  $id = $request->getAttribute('id');
+
+  $title = $request->getParam('title');
+  $category_id = $request->getParam('category_id');
+  $body = $request->getParam('body');
+
+  $sql = "UPDATE posts SET
+          title = :title,
+          category_id = :category_id,
+          body = :body
+          WHERE id = $id";
+
+  try {
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(":title", $title);
+    $stmt->bindParam(":category_id", $category_id);
+    $stmt->bindParam(":body", $body);
+
+    $stmt->execute();
+
+    echo '{"notice":{"text":"Post '. $id .' Updated"}}';
+
+  } catch(PDOException $e){
+    echo '{"error": {"text":'.$e->getMessage().'}}';
+  }
+});
+//DELETE post
+$app->delete('/api/post/delete/{id}', function(Request $request, Response $response)
+{
+  $id = $request->getAttribute('id');
+
+  $sql = "DELETE FROM posts WHERE id = $id";
+
+  try {
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
+    echo '{"notice":{"text":"Post '. $id .' Deleted"}}';
+
+  } catch(PDOException $e){
+    echo '{"error": {"text":'.$e->getMessage().'}}';
+  }
+  });
